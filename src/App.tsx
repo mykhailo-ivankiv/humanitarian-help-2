95bfb812d59cafd2a2aa7bd7b45fde89a28d618d
Mykhailo <neformal.lviv@gmail.com>
2022-03-06 19:02:42 +0100

    Add contacts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, HashRouter } from 'react-router-dom'
 import DropPoints from './DropPoints'
 import './App.css'
 import BEM from './helpers/BEM'
+import 'antd/dist/antd.css'
 const b = BEM('App')
 
 const App = () => {
@@ -10,8 +11,20 @@ const App = () => {
     <HashRouter>
       <section className={b()}>
         <nav className={b('nav')}>
-          Humanitarian help in Netherlands <br />
-          drop points
+          <h1>Humanitarian help in Netherlands</h1>
+
+          <dl>
+            <dd>
+              <a href="mailto:support@ukrainians.nl">support@ukrainians.nl</a>
+            </dd>
+
+            <dt>📞 from 10:00 to 20:00</dt>
+            <dd>
+              <a href="tel:+31619224144">+31 619224144</a> Natalia
+              <br />
+              <a href="tel:+31 682892712">+31 682892712</a> Nikita
+            </dd>
+          </dl>
         </nav>
         <Routes>
           <Route path="demo" element={<MainMapDemo />} />
