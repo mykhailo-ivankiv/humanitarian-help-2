7408bd1994a1d159e5f7d8d3f46d20ab3d43c428
Mykhailo <neformal.lviv@gmail.com>
2022-03-06 10:35:29 +0100

    Save storage id in url.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-// @ts-nocheck
-/* eslint-disable */
-import MainMapDemo from "./MainMapDemo";
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import DropPoints from "./DropPoints";
-import "./App.css";
-import BEM from "./helpers/BEM";
-const b = BEM("App");
+import MainMapDemo from './MainMapDemo'
+import { Routes, Route, HashRouter } from 'react-router-dom'
+import DropPoints from './DropPoints'
+import './App.css'
+import BEM from './helpers/BEM'
+const b = BEM('App')
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <section className={b()}>
-        <nav className={b("nav")}>
+        <nav className={b('nav')}>
           Humanitarian help in Netherlands <br />
           drop points
-          {/*<Link className={b("link")} to={"/demo"}>*/}
-          {/*  Demo*/}
-          {/*</Link>*/}
-          {/*<Link className={b("link")} to="/drop-points">*/}
-          {/*  Drop points Amsterdam*/}
-          {/*</Link>*/}
         </nav>
         <Routes>
-          <Route path="/demo" element={<MainMapDemo />} />
-          <Route path="/drop-points" element={<DropPoints />} />
+          <Route path="demo" element={<MainMapDemo />} />
+          <Route path="drop-points" element={<DropPoints />} />
 
-          <Route path="/*" element={<DropPoints />} />
+          <Route path="/">
+            <Route path="/:storageId" element={<DropPoints />} />
+            <Route path="" element={<DropPoints />} />
+          </Route>
         </Routes>
       </section>
-    </BrowserRouter>
-  );
-};
+    </HashRouter>
+  )
+}
 
-export default App;
+export default App
--- src/DropPoints.tsx
+++ src/DropPoints.tsx
@@ -1,35 +1,37 @@
 // @ts-nocheck
 /* eslint-disable */
-import React, { useState, useReducer, useMemo } from 'react'
+import React, { useState, useMemo } from 'react'
 import Map, { GeolocateControl, Marker, NavigationControl, Popup } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import { useToggle, useAsync } from 'react-use'
-import { StorageType } from './models'
+import { useAsync } from 'react-use'
 import './DropPoints.css'
 import BEM from './helpers/BEM'
 import { bbox, points } from '@turf/turf'
+import { Spin } from 'antd'
+import 'antd/dist/antd.css'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const b = BEM('DropPoints')
 
 function DropPoints() {
-  const [forceRenderCounter, forceRender] = useReducer((x) => x + 1, 0)
-  const [selectedStorage, setSelectedStorage] = useState<StorageType | null>(null)
-  const [mapCenter, setMapCenter] = useState({ longitude: 4.86112597768216, latitude: 52.37672704027482 })
+  const navigate = useNavigate()
+  const { storageId } = useParams()
 
-  const {
-    value: storages = [],
-    // error,
-    loading,
-  } = useAsync(async () => {
+  const { value: storages = [], loading } = useAsync(async () => {
     const docs = await fetch(`${process.env.REACT_APP_API_ENDPOINT}/netherlands-humanitarian-drop-points`)
     return await docs.json()
-  }, [forceRenderCounter])
+  }, [])
+
+  const selectedStorage = useMemo(() => {
+    if (!storageId || storages.length === 0) return null
+
+    return storages.find((storage) => storage.id === storageId)
+  }, [storages, storageId])
 
   const [, setUserCoords] = React.useState({
     latitude: 0,
     longitude: 0,
   })
-  const [showPopup, togglePopup] = useToggle(false)
 
   const bounds = useMemo(() => {
     if (storages.length === 0) return null
@@ -46,84 +48,89 @@ function DropPoints() {
     return [southWest, northEast]
   }, [storages])
 
-  if (loading) return <div>Loading...</div>
-
-  // @ts-ignore
   return (
     <div className={b()}>
-      <div className={b('map')}>
-        <Map
-          mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          initialViewState={{ bounds }}
-          longitude={mapCenter.longitude}
-          latitude={mapCenter.latitude}
-          style={{
-            width: '100%',
-            height: '100%',
-            position: 'sticky',
-            top: 0,
-          }}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-          onMove={({ viewState }) => {
-            setMapCenter({
-              longitude: viewState.longitude,
-              latitude: viewState.latitude,
-            })
-          }}
-        >
-          <NavigationControl />
-          <GeolocateControl
-            onGeolocate={({ coords }) =>
-              setUserCoords({
-                latitude: coords.latitude,
-                longitude: coords.longitude,
-              })
+      <div className={b('map', { loading })}>
+        {loading ? (
+          <Spin className={b('map-spinner')} size="large" />
+        ) : (
+          <Map
+            key={JSON.stringify(bounds)}
+            mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            initialViewState={
+              selectedStorage
+                ? {
+                    longitude: selectedStorage.longitude,
+                    latitude: selectedStorage.latitude,
+                    zoom: 13,
+                  }
+                : {
+                    bounds,
+                    fitBoundsOptions: null,
+                  }
             }
-          />
+            style={{
+              width: '100%',
+              height: '100%',
+              position: 'sticky',
+              top: 0,
+            }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+          >
+            <NavigationControl />
+            <GeolocateControl
+              onGeolocate={({ coords }) =>
+                setUserCoords({
+                  latitude: coords.latitude,
+                  longitude: coords.longitude,
+                })
+              }
+            />
 
-          {storages.map((storage) => {
-            return (
-              <Marker
-                color={storage.status === 'open' ? '#ff0000' : '#A9A9A9'}
-                longitude={storage.longitude}
-                latitude={storage.latitude}
-                // anchor="bottom"
-                onClick={() => {
-                  togglePopup(true)
-                  setSelectedStorage(storage)
+            {storages.map((storage) => {
+              return (
+                <Marker
+                  color={storage.status === 'open' ? '#ff0000' : '#A9A9A9'}
+                  longitude={storage.longitude}
+                  latitude={storage.latitude}
+                  // anchor="bottom"
+                  onClick={() => {
+                    navigate(`/${storage.id}`)
+                  }}
+                  style={{ cursor: 'pointer' }}
+                />
+              )
+            })}
+
+            {selectedStorage && (
+              <Popup
+                closeOnClick={false}
+                offset={[0, -40]}
+                longitude={selectedStorage.longitude}
+                latitude={selectedStorage.latitude}
+                anchor="bottom"
+                onClose={() => {
+                  navigate(`/`)
                 }}
-                style={{ cursor: 'pointer' }}
-              />
-            )
-          })}
+              >
+                {selectedStorage.status === 'open' ? (
+                  <dl>
+                    <dt>Address:</dt>
+                    <dd>{selectedStorage.address}</dd>
 
-          {showPopup && selectedStorage && (
-            <Popup
-              closeOnClick={false}
-              offset={[0, -40]}
-              longitude={selectedStorage.longitude}
-              latitude={selectedStorage.latitude}
-              anchor="bottom"
-              onClose={() => {
-                togglePopup(false)
-              }}
-            >
-              <dl>
-                <dt>Status:</dt>
-                <dd>{selectedStorage.status}</dd>
+                    <dt>Contact:</dt>
+                    <dd>{selectedStorage.contact}</dd>
 
-                <dt>Address:</dt>
-                <dd>{selectedStorage.address}</dd>
-
-                <dt>Contact:</dt>
-                <dd>{selectedStorage.contact}</dd>
-
-                <dt>Reception time:</dt>
-                <dd>{selectedStorage.receptionTime}</dd>
-              </dl>
-            </Popup>
-          )}
-        </Map>
+                    <dt>Reception time:</dt>
+                    <dd>{selectedStorage.receptionTime}</dd>
+                  </dl>
+                ) : (
+                  <span>Temporary not available</span>
+                )}
+              </Popup>
+            )}
+          </Map>
+        )}
       </div>
     </div>
   )
